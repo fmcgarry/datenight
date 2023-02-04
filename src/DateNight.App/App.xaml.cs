bbb8751db7e416d1cd2737723b0d9741db2aedc5
Freddy <freddymcgarry3@gmail.com>
2023-02-04 15:25:08 -0500

    Added new DateNight.App MAUI Blazor project

---
+++ src/DateNight.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DateNight.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ src/DateNight.App/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DateNight.App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
