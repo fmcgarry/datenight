0dc32b55a7a98d6f8c9013bc66cb4b697a650d14
Freddy <freddymcgarry3@gmail.com>
2023-02-04 15:13:27 -0500

    Added Logging adapter

---
+++ src/DateNight.Core/Interfaces/IAppLogger.cs
@@ -0,0 +1,12 @@
+﻿namespace DateNight.Core.Interfaces;
+
+public interface IAppLogger<T>
+{
+    void LogError(string? message, params object[] args);
+
+    void LogInformation(string? message, params object[] args);
+
+    void LogTrace(string? message, params object[] args);
+
+    void LogWarning(string? message, params object[] args);
+}
