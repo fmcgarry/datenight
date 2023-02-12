5b58bf7fda6aef7732b2fe2fe1bc570dd56fedb4
Freddy <freddymcgarry3@gmail.com>
2023-02-12 18:49:37 -0500

    Added data validation to the Create Idea page. Ideas require a Title

--- src/DateNight.App/Models/IdeaModel.cs
+++ src/DateNight.App/Models/IdeaModel.cs
@@ -1,8 +1,13 @@
-﻿namespace DateNight.App.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DateNight.App.Models;
 
 internal class IdeaModel
 {
     public DateTime CreatedOn { get; set; }
+
     public string Description { get; set; }
+
+    [Required]
     public string Title { get; set; }
 }
