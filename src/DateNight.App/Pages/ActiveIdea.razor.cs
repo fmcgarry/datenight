dc09e7c61512bc050da140804b48bd8162c726df
Freddy <freddymcgarry3@gmail.com>
2023-03-05 19:50:25 -0500

    added ActiveIdea page

---
+++ src/DateNight.App/Pages/ActiveIdea.razor.cs
@@ -0,0 +1,23 @@
+﻿using DateNight.App.Interfaces;
+using DateNight.App.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace DateNight.App.Pages;
+
+public partial class ActiveIdea
+{
+    private IdeaModel _idea;
+
+    [Inject]
+    IDateNightApiClient DateNightApiClient { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await GetActiveIdea();
+    }
+
+    private async Task GetActiveIdea()
+    {
+        _idea = await DateNightApiClient.GetActiveIdeaAsync();
+    }
+}
