51fc5605c57b39900831bd1d872f6e0e3710c840
Freddy McGarry <fmcgarry@minitab.com>
2023-03-21 11:51:30 -0400

    Added GetUser and UsersController to API

---
+++ src/DateNight.Api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using DateNight.Api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DateNight.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(string id)
+        {
+            var user = new User()
+            {
+                Name = ""
+            };
+
+            return Ok(user);
+        }
+    }
+}
---
+++ src/DateNight.Api/Data/User.cs
@@ -0,0 +1,7 @@
+﻿namespace DateNight.Api.Data
+{
+    public class User
+    {
+        public required string Name { get; init; }
+    }
+}
