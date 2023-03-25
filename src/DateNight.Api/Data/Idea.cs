f1fd87839177c0f75fc8c3affd73e05de2c787b0
Freddy <freddymcgarry3@gmail.com>
2023-03-25 15:12:06 -0400

    Formatted Idea class

--- src/DateNight.Api/Data/Idea.cs
+++ src/DateNight.Api/Data/Idea.cs
@@ -2,9 +2,9 @@
 
 public class Idea
 {
+    public string CreatedBy { get; set; } = string.Empty;
     public required DateTime CreatedOn { get; init; }
     public required string Description { get; init; }
     public string? Id { get; init; }
     public required string Title { get; init; }
-    public string CreatedBy { get; set; } = string.Empty;
 }
