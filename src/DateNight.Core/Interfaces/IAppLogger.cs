f1bfea0db825b3a2f76ce026ef440874b29b89db
Freddy <freddymcgarry3@gmail.com>
2023-03-25 14:59:23 -0400

    Logging now has method name in log message

--- src/DateNight.Core/Interfaces/IAppLogger.cs
+++ src/DateNight.Core/Interfaces/IAppLogger.cs
@@ -1,14 +1,15 @@
 ﻿namespace DateNight.Core.Interfaces;
 
+[System.Diagnostics.CodeAnalysis.SuppressMessage("Major Code Smell", "S2326:Unused type parameters should be removed", Justification = "Used in inherited classes to instantiate an ILogger<T>")]
 public interface IAppLogger<T>
 {
-    void LogDebug(string? message, params object[] args);
+    void LogDebug(string? message, params object?[] args);
 
-    void LogError(string? message, params object[] args);
+    void LogError(string? message, params object?[] args);
 
-    void LogInformation(string? message, params object[] args);
+    void LogInformation(string? message, params object?[] args);
 
-    void LogTrace(string? message, params object[] args);
+    void LogTrace(string? message, params object?[] args);
 
-    void LogWarning(string? message, params object[] args);
+    void LogWarning(string? message, params object?[] args);
 }
