5bfddf580542b9c2ac68de723644194a2bc6503c
Freddy <freddymcgarry3@gmail.com>
2023-04-16 18:21:30 -0400

    implemented login page logic

--- src/DateNight.App/Pages/Login.razor.cs
+++ src/DateNight.App/Pages/Login.razor.cs
@@ -1,4 +1,6 @@
+using DateNight.App.Interfaces;
 using DateNight.App.Models;
+using Microsoft.AspNetCore.Components;
 
 namespace DateNight.App.Pages;
 
@@ -6,7 +8,15 @@ public partial class Login
 {
     private readonly LoginModel _loginModel = new();
 
-    private void OnValidSubmit()
+    [Inject]
+    public required IDateNightApiClient DateNightApiClient { get; init; }
+
+    [Inject]
+    public required NavigationManager NavigationManager { get; set; }
+
+    private async Task OnValidSubmit()
     {
+        await DateNightApiClient.LoginUserAsync(_loginModel.Email, _loginModel.Password);
+        NavigationManager.NavigateTo("/ideas/active");
     }
 }
