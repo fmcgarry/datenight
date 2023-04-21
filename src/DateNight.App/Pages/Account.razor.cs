6552952246595c5a2d77ac55122f26d446ae381f
Freddy McGarry <fmcgarry@minitab.com>
2023-04-21 10:53:01 -0400

    added disable logic to save button

--- src/DateNight.App/Pages/Account.razor.cs
+++ src/DateNight.App/Pages/Account.razor.cs
@@ -5,8 +5,10 @@ namespace DateNight.App.Pages;
 public partial class Account
 {
     private readonly AccountModel _account = new();
+    private bool _isDirty = false;
 
     private void OnValidSubmit()
     {
+        _isDirty = false;
     }
 }
